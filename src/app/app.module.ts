c4c4252ff9337b0877dec83e208a4da9d40c9e39
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2023-07-09 22:41:47 -0300

    feat: add material e alterado fotos em assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { SwiperModule } from 'swiper/angular';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { ClipboardModule } from 'ngx-clipboard';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { AlertComponent } from './share/alert/alert.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import { DialogComponent } from './share/dialog/dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AlertComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    SwiperModule
+    SwiperModule,
+    NgImageSliderModule,
+    ClipboardModule,
+    MatSnackBarModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ClipboardService } from 'ngx-clipboard';
+import { AlertComponent } from 'src/app/share/alert/alert.component';
+import { DialogComponent } from 'src/app/share/dialog/dialog.component';
 import Swiper from 'swiper';
 
 @Component({
@@ -13,8 +18,17 @@ export class HomeComponent implements OnInit {
   slidesRef: Swiper | undefined;
   sliderAtual = 0;
 
+  imgsWeeding: any[] = [];
+
+  constructor(
+    private _clipboardService: ClipboardService,
+    private _snackBar: MatSnackBar,
+    public dialog: MatDialog
+  ){}
+
   ngOnInit(): void {
     this.proximo();
+    this.gerarLinksImgs();
   }
 
   slideChanged(slides: any) {
@@ -31,12 +45,46 @@ export class HomeComponent implements OnInit {
     this.sliderAtual = this.slidesRef?.realIndex;
   }
 
-  proximo(){
-    setTimeout(()=>{
+  proximo() {
+    setTimeout(() => {
 
       this.slidesRef?.slideNext()
 
-    },6000)
+    }, 6000)
   }
+
+  gerarLinksImgs() {
+    this.imgsWeeding = [];
+    for (let index = 0; index < 39; index++) {
+      const elem = {
+        image: `assets/imgs/pre-wedding/${index}.jpg`,
+        thumbImage: `assets/imgs/pre-wedding/${index}.jpg`,
+        alt: '',
+        title: `foto-${index}.jpg`
+      }
+
+      this.imgsWeeding.push(elem)
+
+    }
+  }
+
+  copyPix(){
+    this._clipboardService.copy('98970278027');
+    this.openSnackBar();
+  }
+
+  openSnackBar(){
+    this ._snackBar.openFromComponent(AlertComponent, {duration: 2000});
+ }
+
+ confirmarPresenca(): void {
+  const dialogRef = this.dialog.open(DialogComponent, {
+    data: {name: 'this.name', animal: 'this.animal'},
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+
+  });
+}
 
 }
